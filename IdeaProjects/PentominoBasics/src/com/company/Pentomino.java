68cce162092df1db2075205dd03457f193e31481
zhecho1215 <zhecho15@yahoo.com>
2018-01-07 14:12:42 +0100

    Update Pentomino.java
    I changed some names and created methods for finding the starting point

--- IdeaProjects/PentominoBasics/src/com/company/Pentomino.java
+++ IdeaProjects/PentominoBasics/src/com/company/Pentomino.java
@@ -6,53 +6,71 @@ package com.company;
 
 public class Pentomino {
 
-    private int[][] letterMat;
-    private int booleanArrPos;
-    private char character;
+    private int[][][] positioningInSpace;
+    private char type;
 
     /**
      Constructor
-     @param Letter the matrix of the pentominoe
-     @param BooArr the position the pentominoe is represented by in the boolean array
-     @param Character the charac the pentominoe represents
+     @param positioningInSpace the position of the pentomino in the space
+     @param c the type the pentomino represents
      */
-    public Pentomino(int[][] Letter, int BooArr, char Character){
-        this.letterMat = Letter;
-        this.booleanArrPos = BooArr;
-        this.character = Character;
+    public Pentomino(int[][][] positioningInSpace, char c){
+        this.positioningInSpace = positioningInSpace;
+        this.type = c;
     }
 
     /**
      * Return LetterMatrix passed to the constructor.
      * @return LetterMatrix
      */
-    public int[][] getLetterMat(){
-        return this.letterMat;
+    public int[][][] getPositioningInSpace(){
+        return this.positioningInSpace;
     }
 
     /**
-     * Return boolean array position passed to the constructor.
-     * @return boolean array position
-     */
-    public int getBooleanArrayPos(){
-        return this.booleanArrPos;
-    }
-
-    /**
-     * Return character passed to the constructor.
+     * Return type passed to the constructor.
      * @return Character */
-    public char getCharacter(){
-        return this.character;
+    public char getType(){
+        return this.type;
     }
 
     /**
      Method that determines which index in the array is considered the starting point for placing the pentomino on the board.
      @return the column index of the first cell in the first row of the matrix that is 1 i.e. not empty
      */
-    public int colIndexOfStartPoint(){
+    public int xIndexOfStartPoint(){
         int index=0;
-        for (int i = 0; i < this.letterMat[0].length; i++) {
-            if(this.letterMat[0][i] == 1) {
+        for (int i = 0; i < this.positioningInSpace.length; i++) {
+            if(this.positioningInSpace[i][0][0] == 1) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    /**
+     Method that determines which index in the array is considered the starting point for placing the pentomino on the board.
+     @return the column index of the first cell in the first row of the matrix that is 1 i.e. not empty
+     */
+    public int yIndexOfStartPoint(){
+        int index=0;
+        for (int i = 0; i < this.positioningInSpace[0].length; i++) {
+            if(this.positioningInSpace[0][i][0] == 1) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+    /**
+     Method that determines which index in the array is considered the starting point for placing the pentomino on the board.
+     @return the column index of the first cell in the first row of the matrix that is 1 i.e. not empty
+     */
+    public int zIndexOfStartPoint(){
+        int index=0;
+        for (int i = 0; i < this.positioningInSpace[0][0].length; i++) {
+            if(this.positioningInSpace[0][0][i] == 1) {
                 index = i;
                 break;
             }
