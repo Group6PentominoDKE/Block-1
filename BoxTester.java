6a2794b990ab9b190d9392b55e820eeb8d1a1d63
LukasPad <34812684+LukasPad@users.noreply.github.com>
2018-01-16 22:41:11 +0100

    Update BoxTester.java

--- BoxTester.java
+++ BoxTester.java
@@ -1,52 +1,75 @@
 import javafx.scene.shape.Box;
 import javafx.scene.paint.*;
-
-
-
+/**
+This Class builds the box shaped parcels one by one and attaches them to the Object
+*/
 public class BoxTester {
 
   private static int RES = 50;
   private int[][][] array;
   private int[] coordList = new int[15];
   private Box box;
-
-  public BoxTester (int[][][] inArray) {
-
-    array = inArray;
-  }
-
-  public int[][][] search (int i, int j, int k, int id) {
-    int TEMPL = 0;
-    int TEMPH = 0;
-    int TEMPW = 0;
-    for(int a=i; a+i<array.length && array[a][j][k] == id; a++) {
-      TEMPL++;
+    /**
+    Constructs the Object for a specific array
+    */
+    public BoxTester (int[][][] inArray) {
+      array = inArray;
     }
-    for(int b=j; b+j<array[0].length && array[i][b][k] == id; b++) {
-      TEMPH++;
-    }
-    for(int c=k; c+k<array[0][0].length && array[i][j][c] == id; c++) {
-      TEMPW++;
-    }
-    build(TEMPL, TEMPH, TEMPW, i, j, k);
-    for(int a=i; a-i < TEMPL; a++) {
-      for(int b=j; b-j < TEMPH; b++) {
-        for(int c=k; c-k < TEMPW; c++) {
-          array[a][b][c] = 0;
+
+    /**
+    Finds the dimensions of a single parcel
+    @param i searched cell's coordinate in the array's first dimension
+    @param j searched cell's coordinate in the array's second dimension
+    @param k searched cell's coordinate in the array's third dimension
+    @param ID number of the parcel
+    @return gives back the array with the parcels array cells cleared
+    */
+    public int[][][] search (int i, int j, int k, int ID) {
+      int TEMPL = 0;
+      int TEMPH = 0;
+      int TEMPW = 0;
+      for(int a=i; a+i<array.length && array[a][j][k] == ID; a++) {
+        TEMPL++;
+      }
+      for(int b=j; b+j<array[0].length && array[i][b][k] == ID; b++) {
+        TEMPH++;
+      }
+      for(int c=k; c+k<array[0][0].length && array[i][j][c] == ID; c++) {
+        TEMPW++;
+      }
+      build(TEMPL, TEMPH, TEMPW, i, j, k);
+      for(int a=i; a-i < TEMPL; a++) {
+        for(int b=j; b-j < TEMPH; b++) {
+          for(int c=k; c-k < TEMPW; c++) {
+            array[a][b][c] = 0;
+          }
         }
       }
+      return array;
     }
-    return array;
-  }
-  public void build(int x, int y, int z, int i, int j, int k) {
-    PhongMaterial material = new PhongMaterial(new Color(Math.random(), Math.random(), Math.random(),1.0));
-    box = new Box(x*RES, y*RES, z*RES);
-    box.setMaterial(material);
-    box.setTranslateX(i*RES);
-    box.setTranslateY(j*RES);
-    box.setTranslateZ(k*RES);
-  }
-  public Box getBox() {
-    return box;
-  }
+    /**
+    Builds and attaches a 3D shape of a random color of the searched parcel to this Object,
+    then moves it into its proper place in the cargo space
+    @param i searched cell's coordinate in the array's first dimension
+    @param j searched cell's coordinate in the array's second dimension
+    @param k searched cell's coordinate in the array's third dimension
+    @param x length of the parcel
+    @param y height of the parcel
+    @param z width of the parcel
+    */
+    public void build(int x, int y, int z, int i, int j, int k) {
+      PhongMaterial material = new PhongMaterial(new Color(Math.random(), Math.random(), Math.random(),1.0));
+      box = new Box(x*RES, y*RES, z*RES);
+      box.setMaterial(material);
+      box.setTranslateX(i*RES);
+      box.setTranslateY(j*RES);
+      box.setTranslateZ(k*RES);
+    }
+    /**
+    Returns the 3D parcel currently attached to this Object
+    @return the box parcel
+    */
+    public Box getBox() {
+      return box;
+    }
 }
