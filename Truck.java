70ab62bf73a7b8977dbdf4c0a65d04feeb888641
LukasPad <34812684+LukasPad@users.noreply.github.com>
2018-01-16 22:39:42 +0100

    Update Truck.java

--- Truck.java
+++ Truck.java
@@ -2,7 +2,6 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.shape.Box;
 import javafx.scene.paint.*;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.transform.Rotate;
@@ -10,27 +9,41 @@ import javafx.event.EventHandler;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
+/**
+This Class is an application which displays
+a rotateable 3D representation of a 3-Dimensional Array
+*/
 public class Truck extends Application {
 
-    private static final int WINDOW_LENGTH = 1500;
-    private static final int WINDOW_HEIGHT = 700;
-    private static final int CameraViewDistance = 700;
-    private static final int RotateAngle = 5;
-    private static final int RES = 50;
-    private static int[][][] array;
-    private static boolean pentMode;
-
+  private static final int WINDOW_LENGTH = 1500;
+  private static final int WINDOW_HEIGHT = 700;
+  private static final int CameraViewDistance = 700;
+  private static final int RotateAngle = 5;
+  private static final int RES = 50;
+  private static int[][][] array;
+  private static boolean pentMode;
+    /**
+    Inputs the array to be represented and
+    whether it will use Box parcels or pentomino parcels
+    @param input integer array containing unique ID's for each parcel
+    @param pent boolean indicating if the cargo uses pentomino prcels
+    */
     public static void insertArray(int[][][] input, boolean pent) {
       array = input;
       pentMode = pent;
     }
+    /**
+    Launches the application, which opens a window containing the collection of stacked parcels,
+    which can then be rotated by the user using M, N and the arrow keys
+    @param stage the top level container of the application
+    */
     public void start(Stage stage) {
 
         Group root = new Group();
         Scene scene = new Scene(root, WINDOW_LENGTH, WINDOW_HEIGHT, true);
-        root.setTranslateX(WINDOW_LENGTH/2 - array.length*RES/2+25);
-        root.setTranslateY(WINDOW_HEIGHT/2 - array[0].length*RES/2+25);
-        root.setTranslateZ(-array[0][0].length*RES/2+25);
+        root.setTranslateX((WINDOW_LENGTH - (array.length-1)*RES)/2);
+        root.setTranslateY((WINDOW_HEIGHT - (array[0].length-1)*RES)/2);
+        root.setTranslateZ((-array[0][0].length+1)*RES/2);
         CargoStacker cargo = new CargoStacker(root, array, pentMode);
         cargo.stack();
 
@@ -59,12 +72,11 @@ public class Truck extends Application {
              if (e.getCode() == KeyCode.M) {
                camera.getTransforms().add(new Rotate(-RotateAngle,WINDOW_LENGTH/2,WINDOW_HEIGHT/2,CameraViewDistance,Rotate.Z_AXIS));
            }
-         }
-        };
-        scene.addEventHandler(KeyEvent.KEY_PRESSED, handler);
-        stage.setTitle("Truck");
-        stage.setScene(scene);
-        stage.show();
-
-}
+        }
+      };
+      scene.addEventHandler(KeyEvent.KEY_PRESSED, handler);
+      stage.setTitle("Truck");
+      stage.setScene(scene);
+      stage.show();
+    }
 }
