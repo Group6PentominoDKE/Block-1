ea83d0c13a33328e059250d37fb123bbbe32378a
CCourtnage <34870661+CCourtnage@users.noreply.github.com>
2018-01-22 12:05:53 +0100

    Add files via upload

--- Filled.java
+++ Filled.java
@@ -1,22 +1,22 @@
 
 public class Filled {
 
-	private char[][][] filled;
+	private String[][][] filled;
 	
 	public Filled(int x, int y, int z){
-		filled = new char[z+1][y+1][x+1];
+		filled = new String[z+1][y+1][x+1];
 		for (int z1 = 0 ; z1 < z+1; z1++)
 			for (int y1 = 0 ; y1 < y+1; y1++)
 				for (int x1 = 0 ; x1 < x+1; x1++) {
-					filled[z1][y1][x1] = 'E';
+					filled[z1][y1][x1] = "0";
 				}
 	}
 	
-	public Filled(char[][][] fill) {
+	public Filled(String[][][] fill) {
 		filled = fill;
 	}
 	
-	public char[][][] getFilled(){
+	public String[][][] getFilled(){
 		return filled;
 	}
 }
